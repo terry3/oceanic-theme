8da7fb69bca332ba3f263aa6ad087b043f7a210c
Tengfei Guo <terryr3rd@yeah.net>
2016-08-01 18:26:07 +0800

    Change highlight color, add pallet.

--- oceanic-theme.el
+++ oceanic-theme.el
@@ -1,6 +1,23 @@
 (deftheme oceanic
   "Oceanic Next Theme")
 
+;; base 00: #1B2B34
+;; base 01: #343D46
+;; base 02: #4F5B66
+;; base 03: #65737E
+;; base 04: #A7ADBA
+;; base 05: #C0C5CE
+;; base 06: #CDD3DE
+;; base 07: #D8DEE9
+;; base 08: #EC5f67
+;; base 09: #F99157
+;; base 0A: #FAC863
+;; base 0B: #99C794
+;; base 0C: #5FB3B3
+;; base 0D: #6699CC
+;; base 0E: #C594C5
+;; base 0F: #AB7967
+
 (custom-theme-set-faces
   'oceanic
   '(default ((t (:foreground "#D8DEE9" :background "#1B2B34"))))
@@ -17,7 +34,7 @@
   '(font-lock-variable-name-face ((t (:foreground "#C594C5" :weight normal))))
   '(minibuffer-prompt ((t (:foreground "#6699CC" :weight normal))))
   '(font-lock-warning-face ((t (:foreground "#EC5f67"  :weight normal))))
-  '(highlight ((t (:background "#D8DEE9" :weight normal))))
+  '(highlight ((t (:background "#343D46" :weight normal))))
   '(linum ((t (:foreground "#AB7967" :weight normal))))
   '(mode-line ((t (:background "#6699CC" :foreground "#1B2B34"
                                :box "#6699CC" :weight normal))))
