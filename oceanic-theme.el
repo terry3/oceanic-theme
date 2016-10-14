673fb3ad35aa3b9e8cef34b983e82b5286732899
terry3 <terryr3rd@yeah.net>
2016-10-14 17:11:40 +0800

    Update description.

--- oceanic-theme.el
+++ oceanic-theme.el
@@ -1,5 +1,35 @@
+;;; oceanic.el --- Oceanic theme for Emacs.
+
+;; Copyright (C) 2016, Tengfei Guo <terryr3rd@yeah.net>
+
+;; Author: Tengfei Guo
+;; Keywords: oceanic color theme emacs
+;; URL: https://github.com/terry3/oceanic-theme
+;; Version: 0.0.1
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 2, or (at your option) any later
+;; version.
+;;
+;; This is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;; for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+;; MA 02111-1307, USA.
+
+;;; Code:
+
 (deftheme oceanic
-  "Oceanic Next Theme")
+  "A color theme based on the Oceanic.")
 
 ;; base 00: #1B2B34
 ;; base 01: #343D46
@@ -44,9 +74,13 @@
   '(show-paren-mismatch ((t (:background "#EC5f67"))))
   )
 
+;; Autoload for MELPA
+
 ;;;###autoload
 (when (and (boundp 'custom-theme-load-path) load-file-name)
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
 
 (provide-theme 'oceanic)
+
+;;; oceanic-theme.el ends here
